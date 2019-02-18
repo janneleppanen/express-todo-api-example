1c196616c32e2e50f63b3633de74972ec3388a8e
Janne Leppänen <janne@janneleppanen.com>
2019-02-18 20:27:24 +0200

    Add getAll query

--- api/tasks.js
+++ api/tasks.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const queries = require("../db/queries");
+
 router.get("/", (req, res) => {
-  res.send({
-    status: "working"
-  });
+  queries.getAll().then(tasks => res.send(tasks));
 });
 
 module.exports = router;
